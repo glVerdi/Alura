e23c3c98041a7766d0f3c9ba4381bc66a131596c
glVerdi <gabrielverdi12@gmail.com>
2024-11-11 18:03:09 -0300

    Acrescentando mais exercicios e alterando o app.js

--- exercicios-js/exercicios.js
+++ exercicios-js/exercicios.js
@@ -50,3 +50,87 @@ alert(`Seu saldo é: ${saldo}`);
 
 let nome2 = prompt("Insira seu nome:");
 alert(`Bem-vindo(a) ${nome}`);
+
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador)
+    contador++;
+}
+
+let contador1 = 10;
+while (contador1 >= 0) {
+    console.log(contador1);
+    contador1--;
+}
+
+let contagemRegressiva = prompt("Digite um número para a contagem regressiva")
+while (contagemRegressiva >= 0) {
+    console.log(contagemRegressiva);
+    contagemRegressiva--;
+}
+
+let contador2 = 0;
+let contagemProgressiva = prompt("Digite um número para a contagem progressiva")
+while (contador2 <= contagemProgressiva) {
+    console.log(contador2);
+    contador2++;
+}
+
+console.log("Boas vindas");
+
+let nome3 = "Gabriel";
+console.log(`Olá, ${nome3}`);
+
+let nome4 = "Gabriel";
+alert(`Olá, ${nome4}`);
+
+let linguagemProgramacao = prompt("Qual a linguagem de programação que você mais gosta?");
+console.log(linguagemProgramacao);
+
+let valor1 = 25;
+let valor2 = 46;
+let resultado = valor1 + valor2;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+let valor3 = 87;
+let valor4 = 93;
+let resultado1 = valor3 - valor4;
+console.log(`A diferença entre ${valor3} e ${valor4} é igual a ${resultado1}`);
+
+let idade3 = prompt("Qual a sua idade?");
+if (idade3 >= 18) {
+    console.log("Você é maior de idade");
+} else {
+    console.log("Você é menor de idade")
+}
+
+let numero1 = prompt("Digite um valor");
+if (numero1 > 0) {
+    console.log("O número é positivo");
+} else if (numero < 0) {
+    console.log("O número é negativo");
+} else {
+    console.log("O número é igual a zero");
+}
+
+let contador3 = 1;
+while (contador3 <= 10) {
+    console.log(contador3);
+    contador++
+}
+
+let nota = 7;
+if (nota >= 7) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
+
+let aleatorio = Math.random();
+console.log(aleatorio);
+
+let aleatorio1A10 = parseInt(Math.random * 10) + 1;
+console.log(aleatorio1A10);
+
+let aleatorio1A1000 = parseInt(Math.random * 1000) + 1;
+console.log(aleatorio1A1000);
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,30 @@
 alert("Boas vindas ao jogo do número secreto"); //Exibe uma mensagem na tela
-let numeroSecreto = 4; //Variável
-console.log(numeroSecreto); //visualização pelo console, que está no inspecionar
-let chute = prompt("Escolha um número entre 1 e 10"); //Pede para a pessoa digitar algo
+let numeroSecreto = parseInt(Math.random() * 100) + 1; //Geração de números pseudo aleatótios, pegando inteiro de 1 até 100
+console.log(numeroSecreto); //visualização pelo console, que está no inspecionar, isso é uma boa prática para fazer testes
+let chute; //Variável
+let tentativas = 1;
 
-if (chute == numeroSecreto) { //Se chute for igual ao número secreto
-    alert(`Isso ai, você dewscobriu o número secreto ${numeroSecreto}`); //Concateção utilizando Template Strings
-} else {
-    alert("Você errou :(");
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 100"); //Pede para a pessoa digitar algo
+    if (chute == numeroSecreto) { 
+        break;
+
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas ++;
+    }
 }
 
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa" // operação ternário 
+alert(`Isso ai, você dewscobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`); //Concateção utilizando Template Strings
+
+/*if (tentativas > 1) {
+    alert(`Isso ai, você dewscobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`); //Concateção utilizando Template Strings
+} else {
+    alert(`Isso ai, você dewscobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`); //Concateção utilizando Template Strings
+}*/
