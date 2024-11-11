c1d4f73f2bffb185d878c134084fb42cb9c9acbf
glVerdi <gabrielverdi12@gmail.com>
2024-11-11 13:37:35 -0300

    Criando o app.js
    O HTML, o CSS e as imagens ja estavam criados.

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto"); //Exibe uma mensagem na tela
+let numeroSecreto = 5;//Variável
+let chute = prompt("Escolha um número entre 1 e 10"); //Pede para a pessoa digitar algo
+
+if (chute == numeroSecreto) {
+    console.log("Isso ai, você dewscobriu o número secreto (5)"); //visualização pelo console, que está no inspecionar
+}
