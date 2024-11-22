3609ce4955262f7b93f6b16de902a987eb4edb05
glVerdi <gabrielverdi12@gmail.com>
2024-11-22 18:13:48 -0300

    deploy do projeto e integração com o front

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
-const app = express()
+const app = express();
+app.use(express.static("/uploads"));
 routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console
--- instalike-back/src/models/postsModel.js
+++ instalike-back/src/models/postsModel.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 // Conecta ao banco de dados usando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -16,4 +18,11 @@ export async function criarPost(novoPost) {
     const db = conexao.db("imersao-instabytes");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const ObjID = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(ObjID)}, {$set:novoPost});
 }
