7a22ab84a32c40b8dc73ca8857610300c55f97e4
glVerdi <gabrielverdi12@gmail.com>
2025-06-16 11:28:42 -0300

    alterando limite para 100

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -5,7 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um númrto entre 1 e 10';
 
 let listaDeNumeroSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -24,7 +24,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
